fc99dfcfcc129189dd9d2c8f6d28a1f99e9691ce
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-06-12 16:22:04 -0400

    Javascript Calculator Challenge
    Javascript Calculator Challenge
    Javascript test

---
+++ Javascript Calculator Challenge/test:indexTest.js
@@ -0,0 +1,7 @@
+1) basic math functions
+add(a, b) adds two numbers and returns the result:
+Error: Expected 'something' to equal 853
+at assert (node_modules/expect/lib/assert.js:29:9)
+at Expectation.toEqual (node_modules/expect/lib/Expectation.js:81:30)
+(test/indexTest.js:26:23)
+at processImmediate (node:internal/timers:464:21)
